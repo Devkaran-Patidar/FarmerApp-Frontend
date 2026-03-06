35230ee4225ae3e92b2b404956bf685c3413ed1f
Devkaran-Patidar <pdevkaran023@gmail.com>
2026-03-06 23:49:58 +0530

    msg

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 // export const API_KEY = process.env.REACT_APP_API_KEY;
-// export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 // export const API_URL = 'http://localhost:8000/api/';
-export const API_URL = "https://farmerapp-backend-jhru.onrender.com";
+// export const API_URL = "https://farmerapp-backend-jhru.onrender.com";

bdaf8b0b0dbc148deb791f356bb82ae91feb74d6
Devkaran-Patidar <pdevkaran023@gmail.com>
2026-03-08 18:07:30 +0530

    add

--- src/Pages/Home_pages/Contact.jsx
+++ src/Pages/Home_pages/Contact.jsx
@@ -5,48 +5,55 @@ import { Mail, Phone, MapPin, MessageCircle } from "lucide-react";
 
 export default function Contact() {
   return (
-    <div className="contact-container">
+    <section className="contact-containery">
       <div className="contact-header">
         <h1>Get in Touch</h1>
         <p>Have questions? We'd love to hear from you</p>
       </div>
 
-     <div className="contact-cards">
-        {/* Email Card */}
+      <div className="contact-cards">
+
+        {/* Email */}
         <div className="contact-card">
           <div className="icon email">
-            <Mail size={30} />
+            <Mail size={28} />
           </div>
           <h3>Email Us</h3>
           <p className="highlight">infoagromart@gmail.com</p>
         </div>
 
-        {/* Call Card */}
+        {/* Phone */}
         <div className="contact-card">
           <div className="icon phone">
-            <Phone size={30} />
+            <Phone size={28} />
           </div>
           <h3>Call Us</h3>
           <p className="highlight">+91 9689858687</p>
         </div>
 
-         <div className="contact-card">
-          <div className="icon phone">
-          <MapPin size={30}/>
+        {/* Location */}
+        <div className="contact-card">
+          <div className="icon location">
+            <MapPin size={28} />
           </div>
           <h3>Our Location</h3>
           <p className="highlight">SVVV, Indore, Madhya Pradesh, India</p>
         </div>
 
-        {/* Working Hours Card */}
+        {/* Message */}
         <div className="contact-card mess">
-          <div className="icon clock">
-            <MessageCircle size={30}/>
+          <div className="icon message">
+            <MessageCircle size={28} />
           </div>
-          <Link to ="contactform" ><h3 className="mess">Mesage Here..</h3></Link>
+
+          <Link to="/contactform">
+            <h3 className="mess">Message Here..</h3>
+          </Link>
+
           <p className="highlight">24/7 - Always Available</p>
         </div>
+
       </div>
-    </div>
+    </section>
   );
 }
--- src/Pages/Home_pages/Product.jsx
+++ src/Pages/Home_pages/Product.jsx
@@ -1,6 +1,11 @@
 import "./Product.css";
 import img1 from "../../assets/product_image/ghee.png"
-import img2 from "../../assets/product_image/honey.jpeg"
+import img2 from "../../assets/product_image/honey2.png"
+import img3 from "../../assets/product_image/image.png"
+import img4 from "../../assets/product_image/image copy.png"
+import img5 from "../../assets/product_image/image copy 5.png"
+import img6 from "../../assets/product_image/image copy 3.png"
+
 export default function Product() {
   return (
     <section className="product-section">
@@ -11,48 +16,59 @@ export default function Product() {
       <div className="product-cards">
 
           <div className="pro-cards">
-              {/* <h1>🍄</h1>
-              <h4>Vegetables</h4>
-              <p>Fresh & Organic</p> */}
-             <div className="pro-img">
-                 <img src={img1} alt="img-1" />
-             </div>
-              <h4>Ghee</h4>
-              <p>Fresh & Organic</p>
+            <div className="pro-img">
+                <img src={img5} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Fruits</h4>
+                <p>Fresh & Organic</p>
+            </div>
           </div>
           <div className="pro-cards">
-               <div className="pro-img">
-                 <img src={img2} alt="img-1" />
-             </div>
-              <h4>Fruits</h4>
-              <p>Seasonal & Imported</p>
+            <div className="pro-img">
+                <img src={img2} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Honey</h4>
+                <p>Natural sweetener</p>
+            </div>
           </div>
           <div className="pro-cards">
-              <h1>🥬</h1>
-              <h4>Leafy Greens</h4>
-              <p>Daily Essential Greens</p>
+            <div className="pro-img">
+                <img src={img3} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Mushroom</h4>
+                <p>Fresh & Organic</p>
+            </div>
           </div>
           <div className="pro-cards">
-              <h1>🌱</h1>
-              <h4>Sprouts</h4>
-              <p>Health & Nutrition Boost</p>
+            <div className="pro-img">
+                <img src={img4} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Vermicompost</h4>
+                <p>organic manure produced by earthworms</p>
+            </div>
           </div>
           <div className="pro-cards">
-              <h1>🥦</h1>
-              <h4>Vegetables</h4>
-              <p>Fresh & Organic</p>
+            <div className="pro-img">
+                <img src={img1} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Fruits</h4>
+                <p>Fresh & Organic</p>
+            </div>
           </div>
           <div className="pro-cards">
-              <h1>🫑</h1>
-              <h4>Vegetables</h4>
-              <p>Fresh & Organic</p>
+            <div className="pro-img">
+                <img src={img6} alt="img-1" />
+            </div>
+            <div className="pro-cont">
+                <h4>Vegitabels</h4>
+                <p>Edible plants</p>
+            </div>
           </div>
-          {/* <div className="pro-cards">
-              <h1>🍎</h1>
-              <h4>Vegetables</h4>
-              <p>Fresh & Organic</p>
-          </div> */}
-
 
       </div>
       
